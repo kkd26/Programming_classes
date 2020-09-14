539e951ff09c8129309300412420d466c6ba6863
Krzysztof Druciarek <kkd26@cam.ac.uk>
2020-09-14 18:27:34 +0200

    Hello World

---
+++ lesson1/helloworld.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // including input output stream library - enables us to use 'cout' and 'cin' command
+
+using namespace std; // use of standare namespace - more later
+
+int main(){ // the main function - any cpp program has to have one
+
+    cout<<"Hello World"; // printing text "Hello world" out into the console
+
+    return 0; // returning 0 - inticator of no errors in the main function
+}
